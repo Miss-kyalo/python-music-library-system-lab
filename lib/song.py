e87d8a1ca3a22dc0cbdf382b2ce0e39ab5bd632d
Miss-kyalo <bellakyalo@gmail.com>
2026-09-09 08:00:11 +0300

    Implement Song class with class attributes and methods for tracking analytics

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,55 @@
+
+
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
+
+    def __init__(self, name, artist, genre):
+        """Initialize instance attributes and invoke tracking class methods."""
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+       
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artists_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        """Increment the total count of songs created."""
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Add unique genre to the genres list."""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Add unique artist to the artists list."""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Increment the genre count dictionary or initialize a new entry."""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        """Increment the artist count dictionary or initialize a new entry."""
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
