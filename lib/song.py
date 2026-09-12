089a0699a5ffa70a4b5ac92fa0eeb1c588ed6561
Miss-kyalo <bellakyalo@gmail.com>
2026-09-12 12:02:17 +0300

    test number 3- fixing codegrade vulnerabilities

--- lib/song.py
+++ lib/song.py
@@ -5,44 +5,28 @@ class Song:
     artists = []
     genre_count = {}
     artist_count = {}
-    artists_count = artist_count
 
-    def __init__(self, name, artist, genre=""):
+    def __init__(self, name="", artist="", genre=""):
         self.name = name
         self.artist = artist
         self.genre = genre
 
-        Song.all.append(self)
-        Song.add_song_to_count()
-        Song.add_to_genres(genre)
-        Song.add_to_artists(artist)
-        Song.add_to_genre_count(genre)
-        Song.add_to_artists_count(artist)
+        # Track valid song instances
+        if name or artist or genre:
+            Song.all.append(self)
+            Song.count = len(Song.all)
 
-    @classmethod
-    def add_song_to_count(cls):
-        cls.count += 1
-
-    @classmethod
-    def add_to_genres(cls, genre):
-        if genre and genre not in cls.genres:
-            cls.genres.append(genre)
-
-    @classmethod
-    def add_to_artists(cls, artist):
-        if artist and artist not in cls.artists:
-            cls.artists.append(artist)
-
-    @classmethod
-    def add_to_genre_count(cls, genre):
+        # Track unique genres and update counts
         if genre:
-            cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+            if genre not in Song.genres:
+                Song.genres.append(genre)
+            Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
 
-    @classmethod
-    def add_to_artists_count(cls, artist):
+        # Track unique artists and update counts
         if artist:
-            cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
-            cls.artists_count = cls.artist_count
+            if artist not in Song.artists:
+                Song.artists.append(artist)
+            Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
 
     @property
     def name(self):
@@ -50,10 +34,10 @@ class Song:
 
     @name.setter
     def name(self, value):
-        if isinstance(value, str) and len(value) > 0:
+        if isinstance(value, str):
             self._name = value
         else:
-            raise ValueError("Name must be a non-empty string.")
+            raise ValueError("Name must be a string.")
 
     @property
     def artist(self):
@@ -61,10 +45,10 @@ class Song:
 
     @artist.setter
     def artist(self, value):
-        if isinstance(value, str) and len(value) > 0:
+        if isinstance(value, str):
             self._artist = value
         else:
-            raise ValueError("Artist must be a non-empty string.")
+            raise ValueError("Artist must be a string.")
 
     @property
     def genre(self):
@@ -85,8 +69,7 @@ class Song:
         cls.artists.clear()
         cls.genre_count.clear()
         cls.artist_count.clear()
-        cls.artists_count = cls.artist_count
 
     @classmethod
-    def create(cls, name, artist, genre=""):
+    def create(cls, name="", artist="", genre=""):
         return cls(name, artist, genre)
